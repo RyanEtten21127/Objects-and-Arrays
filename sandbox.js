6f48234cef337e5cf91526f7cd4af4e4c1fff968
Ryan Etten <retten21127@columbushs.com>
2020-12-06 18:51:13 -0500

    video 5.40

--- sandbox.js
+++ sandbox.js
@@ -6,18 +6,21 @@ let user = {
   email: 'crystal@thenetninja.co.uk',
   location: 'berlin',
   blogs: ['why mac & cheese rules', '10 things to make with marmite'],
-  login: function(){
+  login(){
     console.log('the user logged in');
   },
-  logout: function(){
+  logout(){
     console.log('the user logged out');
   },
-  logBlogs: function(){
+  logBlogs(){
+    // console.log(this.blogs);
+    console.log('this user has written the following blogs:');
+    this.blogs.forEach(blog => {
+      console.log(blog);
+    })
+    console.log(this);
   }
 };
 
-user.login();
-user.logout();
-
-const name = 'mario';
-name.toUpperCase();
+user.logBlogs();
+console.log(this);
